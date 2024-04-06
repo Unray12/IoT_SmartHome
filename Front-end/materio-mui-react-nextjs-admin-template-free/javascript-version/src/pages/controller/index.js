97e7c0528bdc17bb510ae816991f9af3f1a7e6ac
ThienNguyen15 <nguyenquangthien1506@gmail.com>
2024-04-06 13:58:39 +0700

    Update front-end

--- Front-end/materio-mui-react-nextjs-admin-template-free/javascript-version/src/pages/controller/index.js
+++ Front-end/materio-mui-react-nextjs-admin-template-free/javascript-version/src/pages/controller/index.js
@@ -26,12 +26,15 @@ const Controller = () => {
   return (
     <Grid container spacing={6}>
       <Grid item xs={12}>
-        <Typography variant='h5'>
-          <Link href='https://materialdesignicons.com/' target='_blank'>
-            Material Design Icons
-          </Link>
-        </Typography>
-        <Typography variant='body2'>Material Design Icons from the Community</Typography>
+      <Typography variant='h4' style={{ display: 'inline-block', verticalAlign: 'middle', lineHeight: '28px' }}>
+        <Link href='https://materialdesignicons.com/' target='_blank'>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 28 28" width="28" height="28" style={{ verticalAlign: 'middle' }}>
+            <path d="M3,17V19H9V17H3M3,5V7H13V5H3M13,21V19H21V17H13V15H11V21H13M7,9V11H3V13H7V15H9V9H7M21,13V11H11V13H21M15,9H17V7H21V5H17V3H15V9Z" />
+          </svg>
+          &nbsp;Device Controller
+        </Link>
+      </Typography>
+        {/* <Typography variant='body2'>Device Controller</Typography> */}
         {/* {devices.map(device => {
           <span key={device}>
             <BasicCard text={device}/>
