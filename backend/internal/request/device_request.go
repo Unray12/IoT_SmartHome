ba4f904dbcd6ef95e0a741e0a5c74da3d4bd1c5b
nct2309 <nctstuti@gmail.com>
2024-04-14 23:17:31 +0700

    Add General GET API for update any kinds of devices but still need validations for data in request layer

--- backend/internal/request/device_request.go
+++ backend/internal/request/device_request.go
@@ -20,31 +20,30 @@ type deviceRequest struct {
 //api route: /devices/update?house_id=1&device_id=2&device_type=Door&data=30&state=1
 
 func (r *deviceRequest) GetDataFromDeviceRequest(ctx *gin.Context) (int, int, string, float64, bool, error) {
-  // read the request url to extract the query parameters
-  HouseID, _ := ctx.GetQuery("house_id")
-  DeviceID, _ := ctx.GetQuery("device_id")
-  deviceType, _ := ctx.GetQuery("device_type")
-  Data, _ := ctx.GetQuery("data")
-  State, _ := ctx.GetQuery("state")
+	// read the request url to extract the query parameters
+	HouseID, _ := ctx.GetQuery("house_id")
+	DeviceID, _ := ctx.GetQuery("device_id")
+	deviceType, _ := ctx.GetQuery("device_type")
+	Data, _ := ctx.GetQuery("data")
+	State, _ := ctx.GetQuery("state")
 
-  // convert the query parameters to the desired data type
-  houseID, _ := strconv.Atoi(HouseID)
-  deviceID, _ := strconv.Atoi(DeviceID)
-  data, _ := strconv.ParseFloat(Data, 64)
-  state, _ := strconv.ParseBool(State)
+	// convert the query parameters to the desired data type
+	houseID, _ := strconv.Atoi(HouseID)
+	deviceID, _ := strconv.Atoi(DeviceID)
+	data, _ := strconv.ParseFloat(Data, 64)
+	state, _ := strconv.ParseBool(State)
 
+	// if deviceType == "Door" {
 
-  if deviceType == "Door" {
-  
-  } else if deviceType == "Temperature" {
+	// } else if deviceType == "Temperature" {
 
-  } else if deviceType == "Humidity" {
+	// } else if deviceType == "Humidity" {
 
-  } else if deviceType == "Fan" {
+	// } else if deviceType == "Fan" {
 
-  } else if deviceType == "Light" {
+	// } else if deviceType == "Light" {
 
-  }
+	// }
 
-  return houseID, deviceID, deviceType, data, state, nil
+	return houseID, deviceID, deviceType, data, state, nil
 }
