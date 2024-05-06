a1d511f9f8d0efe4f7466e211cad188cbc0914c8
Tran Minh Triet <tranminhtriet0@gmail.com>
2024-05-06 13:04:22 +0700

    fix actlogtable height

--- Front-end/materio-mui-react-nextjs-admin-template-free/javascript-version/src/pages/activity-log/ActLogTable.js
+++ Front-end/materio-mui-react-nextjs-admin-template-free/javascript-version/src/pages/activity-log/ActLogTable.js
@@ -91,7 +91,7 @@ const ActLogTable = () => {
 
   return (
     <Paper sx={{ width: '100%', overflow: 'hidden' }}>
-      <TableContainer sx={{ maxHeight: 440 }}>
+      <TableContainer sx={{ maxHeight: 550 }}>
         <Table stickyHeader aria-label="sticky table">
           <TableHead>
             <TableRow>
