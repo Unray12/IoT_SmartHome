ba47cc5fb13f7d7bb8bfd06392d3a577fbdb8588
treerspeaking <93274119+treerspeaking@users.noreply.github.com>
2024-05-08 01:49:12 +0700

    Feat: Fix turn on-off bug

--- Front-end/materio-mui-react-nextjs-admin-template-free/javascript-version/src/pages/controller/DeviceProvider.js
+++ Front-end/materio-mui-react-nextjs-admin-template-free/javascript-version/src/pages/controller/DeviceProvider.js
@@ -20,8 +20,8 @@ export const DeviceProvider = ({ children }) => {
                 Authorization: localStorage.getItem('SavedToken'),
               },
             });
-            
-    
+
+
             const data = response.data;
             setLightChecked(data.light);
             setFanChecked(data.fan);
@@ -33,15 +33,63 @@ export const DeviceProvider = ({ children }) => {
             // Handle errors gracefully
           }
         };
-    
+
         fetchData(); // Fetch data immediately for testing
-    
-        const intervalId = setInterval(fetchData, 5000); // Fetch data every 8 seconds
-    
-        return () => clearInterval(intervalId); // Cleanup on component unmount
+
+        // remove set fetch interval, this code only run when component mount
       }, []); // Empty dependency array ensures data is fetched only once on component mount
 
-    return ( 
+      useEffect(() => {
+        if (fanChecked) {
+          const BackendLink = 'https://hgs-backend.onrender.com';
+          const fetchFanData = async () => {
+        try {
+          const response = await axios.get(`${BackendLink}/users/getDashboardData`, {
+            headers: {
+          Authorization: localStorage.getItem('SavedToken'),
+            },
+          });
+          const data = response.data;
+          // setFanChecked(data.fan);
+          setFanLevel(data.fan_speed);
+        } catch (error) {
+          console.error('Error fetching data:', error);
+          // Handle errors gracefully
+        }
+          };
+          fetchFanData(); // Fetch data immediately for testing
+
+          // remove fetch interval this code will only run when fanchecked changes
+        }
+      }, [fanChecked]); // Empty dependency array ensures data is fetched only once on component mount
+
+
+      useEffect(() => {
+        if (lightChecked) {
+          const BackendLink = 'https://hgs-backend.onrender.com';
+          const fetchLightData = async () => {
+        try {
+          const response = await axios.get(`${BackendLink}/users/getDashboardData`, {
+            headers: {
+          Authorization: localStorage.getItem('SavedToken'),
+            },
+          });
+          const data = response.data;
+          // setLightChecked(data.light);
+          setLightLevel(data.light_level);
+        } catch (error) {
+          console.error('Error fetching data:', error);
+          // Handle errors gracefully
+        }
+          };
+
+          fetchLightData(); // Fetch data immediately for testing
+
+          // remove fetch interval this code only run when lightchecked changed
+        }
+      }, [lightChecked]); // depency, only change when lightChecked changes
+
+    return (
     <deviceContext.Provider value={{ lightChecked, setLightChecked, fanChecked, setFanChecked, fanLevel, setFanLevel, doorOpen, setDoorOpen, lightLevel, setLightLevel }}>
         {children}
     </deviceContext.Provider>
